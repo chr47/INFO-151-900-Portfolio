74592debf532c130de3413481ba0e6630ab89608
chr47 <chr47@drexel.edu>
2025-06-11 19:27:25 -0400

    consolidating js, fixing dermascope aos mobile interaction, finishing touches

--- js/aos.js
+++ js/aos.js
@@ -9,7 +9,23 @@ const aosEffects = [
   "fade-down-left",
 ];
 
-document.querySelectorAll("img").forEach((img) => {
-  const effect = aosEffects[Math.floor(Math.random() * aosEffects.length)];
-  img.setAttribute("data-aos", effect);
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelectorAll("img:not(.icon)").forEach((img) => {
+    const effect = aosEffects[Math.floor(Math.random() * aosEffects.length)];
+    img.setAttribute("data-aos", effect);
+  });
+
+  AOS.init({
+    duration: 800,
+    once: true,
+    offset: 10,
+  });
+});
+
+window.addEventListener("load", () => {
+  AOS.refresh();
+});
+
+window.addEventListener("pageshow", () => {
+  AOS.refreshHard();
 });
